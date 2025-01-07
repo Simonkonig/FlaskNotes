09f22ca005299272f23d6d9e6ca3ff50d22d5610
Simonkonig <simonkonigsfeldt@gmail.com>
2025-01-07 15:24:08 +0100

    1st

--- source/website/__init__.py
+++ source/website/__init__.py
@@ -5,10 +5,8 @@ def create_app():
     app.config['SECRET'] = 'huighisuahdf asfduh'
 
 
-    from.views import views
-    from.auth import auth
-
-
+    from .views import views
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
